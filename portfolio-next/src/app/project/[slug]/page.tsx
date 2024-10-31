081fea002db112a915a162fec7b6de2e770c91e3
Eirik Kirkeli <knirkeli88@msn.com>
2024-10-31 23:02:31 +0100

    Some more typing fixes

--- portfolio-next/src/app/project/[slug]/page.tsx
+++ portfolio-next/src/app/project/[slug]/page.tsx
@@ -1,91 +1,8 @@
 "use client";
-// import { useRouter } from "next/router";
-// import { useEffect, useState } from "react";
-// import { createClient } from "@sanity/client";
-// import imageUrlBuilder from "@sanity/image-url";
-
-// const client = createClient({
-//   projectId: "2rar6jsc",
-//   dataset: "production",
-//   useCdn: true,
-// });
-
-// const builder = imageUrlBuilder(client);
-
-// function urlFor(source) {
-//   return builder.image(source);
-// }
-
-// const fetchProjectBySlug = async (slug) => {
-//   const query = `*[_type == "projectPost" && slug.current == $slug][0]{
-//     header,
-//     slug,
-//     description,
-//     images,
-//     category->{
-//       title
-//     },
-//     completionDate,
-//     inProgress
-//   }`;
-
-//   try {
-//     const project = await client.fetch(query, { slug });
-//     return project;
-//   } catch (error) {
-//     console.error("Error fetching project:", error);
-//     return null;
-//   }
-// };
-
-// const ProjectPage = () => {
-//   const router = useRouter();
-//   const { slug } = router.query;
-//   const [project, setProject] = useState(null);
-
-//   useEffect(() => {
-//     if (slug) {
-//       fetchProjectBySlug(slug).then((data) => setProject(data));
-//     }
-//   }, [slug]);
-
-//   if (!project) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div className="max-w-2xl mx-auto p-4">
-//       <h1 className="text-3xl font-bold mb-4">{project.header}</h1>
-//       {project.images && project.images.length > 0 && (
-//         <img
-//           className="w-full h- object-cover mb-4"
-//           src={urlFor(project.images[0]).url()}
-//           alt={`${project.header} image`}
-//         />
-//       )}
-//       <p className="text-gray-700 text-base mb-4">{project.description}</p>
-//       {project.completionDate && (
-//         <p className="text-gray-500 text-sm mb-4">
-//           Ferdigstillt {project.completionDate}
-//         </p>
-//       )}
-//       {project.inProgress && (
-//         <p className="text-gray-500 text-sm mb-4">
-//           Under arbeid: {project.inProgress ? "Ja" : "Nei"}
-//         </p>
-//       )}
-//       <p className="text-gray-500 text-sm mb-4">
-//         Kategori: {project.category?.title}
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default ProjectPage;
-
 import { useEffect, useState } from "react";
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const client = createClient({
   projectId: "2rar6jsc",
@@ -95,11 +12,11 @@ const client = createClient({
 
 const builder = imageUrlBuilder(client);
 
-function urlFor(source) {
+function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
 
-const fetchProjectBySlug = async (slug) => {
+const fetchProjectBySlug = async (slug: unknown) => {
   const query = `*[_type == "projectPost" && slug.current == $slug][0]{
     header,
     slug,
@@ -121,9 +38,23 @@ const fetchProjectBySlug = async (slug) => {
   }
 };
 
-const ProjectPage = ({ params }) => {
+interface Params {
+  slug: string;
+}
+
+const ProjectPage = ({ params }: { params: Params }) => {
   const { slug } = params;
-  const [project, setProject] = useState(null);
+  interface Project {
+    header: string;
+    slug: string;
+    description: string;
+    images: { _key: string; alt?: string }[];
+    category: { title: string };
+    completionDate?: string;
+    inProgress?: boolean;
+  }
+
+  const [project, setProject] = useState<Project | null>(null);
 
   useEffect(() => {
     if (slug) {
